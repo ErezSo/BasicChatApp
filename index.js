a3276fcb3307ec53eddc21a27e63aa178aa6fb2f
Erez Sobel <sobelerez@gmail.com>
2016-06-01 13:34:07 +0200

    Add view template

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ app.get('/', function(reg, res) {
   res.send('Server initialized');
 });
 
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
 console.log('listening on port ', port);
+
+app.set('views', __dirname + '/template');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.get('/', function(req, res) {
+  res.render('page');
+});
