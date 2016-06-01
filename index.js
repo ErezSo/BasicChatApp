7c94aac1e809d3f670854b36a42fef535e896e94
Erez Sobel <sobelerez@gmail.com>
2016-06-01 18:39:22 +0200

    Display messages in the chat

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.get('/', function(req, res) {
 });
 
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('chat message', msg);
-  });
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('message', msg);
+  })
 });
 
 
